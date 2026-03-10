d0b0bf415d96c7c8bae70dfd433a53110797ffa8
ragna3348 <58966752+ragna3348@users.noreply.github.com>
2026-03-10 20:38:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:3001'],
+   origin: [
+    "http://localhost:5173",
+    "https://akademik-v2.vercel.app"
+  ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
